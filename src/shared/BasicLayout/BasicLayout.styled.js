6d4ea6c453f76bb65fd08f1761e28eb2c2affebd
Viacheslav <108462275+ViacheslavYUR@users.noreply.github.com>
2023-03-05 15:21:39 +0200

    style-fix

--- src/shared/BasicLayout/BasicLayout.styled.js
+++ src/shared/BasicLayout/BasicLayout.styled.js
@@ -4,11 +4,13 @@ export const Container = styled.div`
   max-width: 1200px;
   margin-left: auto;
   margin-right: auto;
+  padding: 20px;
 `;
 
 export const Header = styled.header`
   display: flex;
   align-items: center;
+  padding: 20px 10px;
   min-height: 60px;
   box-shadow: rgba(0, 0, 0, 0.36) 0px 5px 10px;
 `;
